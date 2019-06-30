20f5e7c517c3f29424ed31cefa1214a4981b1626
Leonardo Bilia <leonardo.bilia@gmail.com>
2019-06-30 16:39:48 -0300

    Initial Commit

---
+++ FlipCard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FlipCard
+//
+//  Created by Leonardo Bilia on 6/30/19.
+//  Copyright © 2019 Leonardo Bilia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
